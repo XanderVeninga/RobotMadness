551e8ee33dc2bfab637920014bc0a808f8ac59b6
Xander Veninga <39034729+EliteForce1@users.noreply.github.com>
2023-11-24 10:56:58 +0100

    GEKOLONISEERD (nathan)

--- Assets/Scripts/ScriptableObjects/Apliances/PressController.cs
+++ Assets/Scripts/ScriptableObjects/Apliances/PressController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class PressController : AplianceClass
 {
+    public AplianceClass resourceManager;
     // Start is called before the first frame update
     private void Update()
     {
