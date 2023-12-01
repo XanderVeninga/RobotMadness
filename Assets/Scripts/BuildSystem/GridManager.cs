691462ef576a1f8811ba8dd63d97769caeb71b40
Xander Veninga <xanderveninga@gmail.com>
2023-12-01 10:19:24 +0100

    generic grid system

--- Assets/Scripts/BuildSystem/GridManager.cs
+++ Assets/Scripts/BuildSystem/GridManager.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
 
 public class GridManager : MonoBehaviour
 {
-    [SerializeField] Grid grid;
+    [SerializeField] Grid<bool> grid;
 
     private void Start()
     {
-        grid = new Grid(5,5,10f);
+        grid = new Grid<bool>(5,5,10f, new Vector3(0,0,0));
     }
 
-
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), true);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        }
+    }
 }
