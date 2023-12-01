d8744aa74dc7a51711f790d53f2f6be6028155d6
DrArson <95631143+DrArson@users.noreply.github.com>
2023-12-01 10:48:20 +0100

    shit

--- Assets/UI/MainMenu.cs
+++ Assets/UI/MainMenu.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
- public void PlayGame()
+    public void PlayGame()
+    {
         SceneManager.LoadSceneAsync(1);
+    }
+
 }
