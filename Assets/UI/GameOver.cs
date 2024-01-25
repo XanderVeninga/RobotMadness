a75c26b955c07feb6711935f9cd12bb9260e9e00
DrArson <95631143+DrArson@users.noreply.github.com>
2024-01-25 16:50:37 +0100

    added game over

---
+++ Assets/UI/GameOver.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameOver : MonoBehaviour
+{
+    // Update is called once per frame
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void TEST()
+    {
+
+    }
+}
